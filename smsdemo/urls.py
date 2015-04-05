8ae5516140ea57efc5e5addcfd85982a4de0e178
Mark Lavin <mlavin@caktusgroup.com>
2015-04-05 09:13:07 -0400

    Wire up the initial url patterns.

--- smsdemo/urls.py
+++ smsdemo/urls.py
@@ -1,10 +1,22 @@
-from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'smsdemo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from rapidsms.views import dashboard
 
+
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-)
+    # RapidSMS core URLs
+    url(r'^accounts/', include('rapidsms.urls.login_logout')),
+    url(r'^$', dashboard, name='rapidsms-dashboard'),
+    # RapidSMS contrib app URLs
+    url(r'^messagelog/', include('rapidsms.contrib.messagelog.urls')),
+    # Third party URLs
+    url(r'^selectable/', include('selectable.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^httptester/', include('rapidsms.contrib.httptester.urls')),
+    ]
